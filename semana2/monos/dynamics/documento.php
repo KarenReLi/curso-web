a9e92f3a92f744d71424e4923c121ce761c6cfc1
Karen <karenrl2002@gmail.com>
2023-05-22 21:08:41 -0600

    test: añade nueva version de tarea de monos

--- semana2/monos/dynamics/documento.php
+++ semana2/monos/dynamics/documento.php
@@ -19,8 +19,85 @@
                 "<table border='1' style='border-collapse:collapse;' align='center' cellpading='30px'>
                 <thead>
                 <tr >
-                    <th> Libro numero: ";
-            $num = rand ();
-        
+                    <th> Libro: ";
+            for ($i=1; $i<=10; $i++){
+                $numeros = rand (48, 57);
+                $mayuscula = rand (65, 90);
+                $minuscula = rand (97, 122);
+                $valor = rand (1, 3); //32, 48-57, 65-90, 97-122
+                switch ($valor){
+                    case 1:
+                        echo chr ($numeros);
+                        break;
+                    case 2:
+                        echo chr ($mayuscula);
+                        break;
+                    case 3:
+                        echo chr ($minuscula);
+                        break;
+                }
+            }
+            echo
+                "</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                <td>";
+                if ($buscador='normal')
+                {
+                    for ($i=1; $i<=10; $i++){
+                        $espacio = 32;
+                        $numeros = rand (48, 57);
+                        $mayuscula = rand (65, 90);
+                        $minuscula = rand (97, 122);
+                        $ubicacion = rand (5, 10);
+                        $valor = rand (1, 10);
+                        switch ($valor){
+                            case $ubicacion:
+                                echo "<strong>".$escrito."</strong>";
+                                $ubicacion = 0;
+                                break;                            
+                            case 1:
+                                echo chr ($espacio);
+                                break;
+                            case 2:
+                                echo chr ($numeros);
+                                break;
+                            case 3:
+                                echo chr ($mayuscula);
+                                break;
+                            case 4:
+                                echo chr ($minuscula);
+                                break;
+                            
+                        }
+                        }
+                }
+                    
+                    echo "</td>
+                        </tr>
+                        </tbody>";
+                    var_dump ($ubicacion);  
+
+
             
-        ?>
+        
+        /*
+                        var_dump ($valor);
+                        if ($valor == '1'){
+                            echo chr ($espacio);
+                        }
+                        if ($valor == '2'){
+                            echo chr ($numeros);
+                        }
+                        if ($valor == '3'){
+                            echo chr ($mayuscula);
+                        }
+                        if ($valor == '4'){
+                            echo chr ($minuscula);
+                        }
+                        /*if ($valor == $ubicacion){
+                            echo "Hola";
+                        }*/
+                        ?>
